5779ec4ad196554c8a52470266f204ff7c09fcbc
James Aduke <jamesodwuor4@gmail.com>
2022-03-14 07:04:33 +0300

    style navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,11 @@
 import React from "react";
-
-
-
+import Logo from "../images/globe.png";
 
 const Navbar = () => {
   return (
     <nav className="nav">
-        <img src="" alt="Travel Journal Logo" className="nav--logo" />
-        <h3>my TravelJournal</h3>
+        <img src={Logo} alt="travel journal logo" className="nav--logo"/>
+        <h3 className="nav--title">my TravelJournal</h3>
     </nav>
   )
 }
